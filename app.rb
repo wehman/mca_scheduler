2bd81786dce18df3e0dc45aada09a8701073ba08
Max Pokropowicz <Max Pokropowicz>
2017-04-23 14:19:38 -0400

    name and id save to DB, class hash does not

--- app.rb
+++ app.rb
@@ -14,5 +14,30 @@ post "/list" do
 	name = params[:name]
 	id = params[:student_id]
 
+	db_params = {  # AWS db
+	host: ENV['host'],
+	port:ENV['port'],
+	dbname:ENV['dbname'],
+	user:ENV['dbuser'],
+	password:ENV['dbpassword']
+	}
+
+	conn = PG::Connection.new(db_params)
+
+	# determine current max index (id) in details table
+	max_id = conn.exec("select max(id) from students")[0]
+
+	# prepare SQL statement to insert common individual form fields into common table
+	conn.prepare('q_statement',
+				"insert into students
+				(name, student_id, classes)
+				values($1, $2, $3)")  # bind parameters
+
+	# execute prepared SQL statement
+	conn.exec_prepared('q_statement', [name, id, classes])
+
+	# deallocate prepared statement variable (avoid error "prepared statement already exists")
+	conn.exec("deallocate q_statement")
+
 	"#{classes} , #{name}, #{id}"
 end
