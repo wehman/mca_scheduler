3c9c53e2d18fe298cc97a37040a0a45b045d80fd
Brian Lewis <blewis8958@gmail.com>
2017-05-01 11:52:17 -0400

    got rid of all errors but still has hard coded values

--- sorting.rb
+++ sorting.rb
@@ -86,136 +86,167 @@ def sort(array_of_classes, limit)
 
 		counter = @left_over_kids.length
 		(counter).times do |x|
-			student = @left_over_kids[0]
+			if @left_over_kids == [[]]
+				break
+			else
+				student = @left_over_kids[0]
+			
+			
 			# puts "Student: #{student}"
 			# print "\n"
 
-			if (student[0]["2"] == 1) && (@storage1.length < limit)
-				@storage1.push(student[0])
-				@left_over_kids[0].delete(student[0])
-				@left_over_kids = @left_over_kids
-			elsif (student[0]["2"] == 2) && (@storage2.length < limit)
-				@storage2.push(student[0])
-				@left_over_kids[0].delete(student[0])
-				@left_over_kids = @left_over_kids
-			elsif (student[0]["2"] == 3) && (@storage3.length < limit)
-				@storage3.push(student[0])
-				@left_over_kids[0].delete(student[0])
-				@left_over_kids = @left_over_kids
-			elsif (student[0]["2"] == 4) && (@storage4.length < limit)
-				@storage4.push(student[0])
-				@left_over_kids[0].delete(student[0])
-				@left_over_kids = @left_over_kids
-			elsif (student[0]["2"] == 5) && (@storage5.length < limit)
-				@storage5.push(student[0])
-				@left_over_kids[0].delete(student[0])
-				@left_over_kids = @left_over_kids
+				if (student[0]["2"] == 1) && (@storage1.length < limit)
+					@storage1.push(student[0])
+					@left_over_kids[0].delete(student[0])
+					@left_over_kids = @left_over_kids
+				elsif (student[0]["2"] == 2) && (@storage2.length < limit)
+					@storage2.push(student[0])
+					@left_over_kids[0].delete(student[0])
+					@left_over_kids = @left_over_kids
+				elsif (student[0]["2"] == 3) && (@storage3.length < limit)
+					@storage3.push(student[0])
+					@left_over_kids[0].delete(student[0])
+					@left_over_kids = @left_over_kids
+				elsif (student[0]["2"] == 4) && (@storage4.length < limit)
+					@storage4.push(student[0])
+					@left_over_kids[0].delete(student[0])
+					@left_over_kids = @left_over_kids
+				elsif (student[0]["2"] == 5) && (@storage5.length < limit)
+					@storage5.push(student[0])
+					@left_over_kids[0].delete(student[0])
+					@left_over_kids = @left_over_kids
+				end
 			end
 		end
 
 
-		puts "Storage 1:#{@storage1}"
-		puts "Storage 2:#{@storage2}"
-		puts "Storage 3:#{@storage3}"
-		puts "Storage 4:#{@storage4}"
-		puts "Storage 5:#{@storage5}"
-		puts "left over:#{@left_over_kids}"
-		print "\n"
-		print "\n"
+		# puts "Storage 1:#{@storage1}"
+		# puts "Storage 2:#{@storage2}"
+		# puts "Storage 3:#{@storage3}"
+		# puts "Storage 4:#{@storage4}"
+		# puts "Storage 5:#{@storage5}"
+		# puts "left over:#{@left_over_kids}"
+		# print "\n"
+		# print "\n"
 		
-
+		# print @left_over_kids
 		counter = @left_over_kids.length
-		(counter).times do |x|
-			student = @left_over_kids[0]
+		# puts counter
+		# print @left_over_kids
+		(counter).times do
 
-			while student.length != 0
+			# puts @left_over_kids.length
 
-				if (student[0]["3"] == 1) && (@storage1.length < limit)
-					@storage1.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif (student[0]["3"] == 2) && (@storage2.length < limit)
-					@storage2.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif (student[0]["3"] == 3) && (@storage3.length < limit)
-					@storage3.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif (student[0]["3"] == 4) && (@storage4.length < limit)
-					@storage4.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif (student[0]["3"] == 5) && (@storage5.length < limit)
-					@storage5.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
+			if @left_over_kids.length == [[]]
+				break
+			else
+				student = @left_over_kids[0]
+
+				if student == []
+					break
+				else
+					if (student[0]["3"] == 1) && (@storage1.length < limit)
+						@storage1.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif (student[0]["3"] == 2) && (@storage2.length < limit)
+						@storage2.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif (student[0]["3"] == 3) && (@storage3.length < limit)
+						@storage3.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif (student[0]["3"] == 4) && (@storage4.length < limit)
+						@storage4.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif (student[0]["3"] == 5) && (@storage5.length < limit)
+						@storage5.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					end
 				end
 			end
 		end
 
 		counter = @left_over_kids.length
 		(counter).times do |x|
-			student = @left_over_kids[0]
+			if @left_over_kids.length == [[]]
+				break
+			else
+				student = @left_over_kids[0]
+			
+				if student == []
+					break
+				else
 
-			while student.length != 0
-
-				if student[0]["4"] == 1 && @storage1.length < limit
-					@storage1.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif student[0]["4"] == 2 && @storage2.length < limit
-					@storage2.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif student[0]["4"] == 3 && @storage3.length < limit
-					@storage3.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif student[0]["4"] == 4 && @storage4.length < limit
-					@storage4.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif student[0]["4"] == 5 && @storage5.length < limit
-					@storage5.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
+					if student[0]["4"] == 1 && @storage1.length < limit
+						@storage1.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif student[0]["4"] == 2 && @storage2.length < limit
+						@storage2.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif student[0]["4"] == 3 && @storage3.length < limit
+						@storage3.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif student[0]["4"] == 4 && @storage4.length < limit
+						@storage4.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif student[0]["4"] == 5 && @storage5.length < limit
+						@storage5.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					end
 				end
 			end
 		end
 
 		counter = @left_over_kids.length
 		(counter).times do |x|
-			student = @left_over_kids[0]
+			if @left_over_kids.length == [[]]
+				break
+			else
+				student = @left_over_kids[0]
+			
+				if student == []
+					break
+				else
 
-			while student.length != 0
-				if student[0]["5"] == 1 && @storage1.length < limit
-					@storage1.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif student[0]["5"] == 2 && @storage2.length < limit
-					@storage2.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif student[0]["5"] == 3 && @storage3.length < limit
-					@storage3.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif student[0]["5"] == 4 && @storage4.length < limit
-					@storage4.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
-				elsif student[0]["5"] == 5 && @storage5.length < limit
-					@storage5.push(student[0])
-					@left_over_kids[0].delete(student[0])
-					@left_over_kids = @left_over_kids
+					if student[0]["5"] == 1 && @storage1.length < limit
+						@storage1.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif student[0]["5"] == 2 && @storage2.length < limit
+						@storage2.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif student[0]["5"] == 3 && @storage3.length < limit
+						@storage3.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif student[0]["5"] == 4 && @storage4.length < limit
+						@storage4.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					elsif student[0]["5"] == 5 && @storage5.length < limit
+						@storage5.push(student[0])
+						@left_over_kids[0].delete(student[0])
+						@left_over_kids = @left_over_kids
+					end
 				end
 			end
 		end
+
+		@left_over_kids = @left_over_kids.flatten
 
 end
 
-sort(classes, 2)
+sort(classes, 1)
 
 	puts "Class 1:#{@storage1}"
 	print "\n"
